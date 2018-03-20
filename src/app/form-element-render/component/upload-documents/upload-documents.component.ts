05c1d7e7a13819f619c9fe69bc54eaf60920356e
Котрехов Андрей <kotrekhov@platformasoft.ru>
2018-03-20 18:25:11 +0300

    RM-4267: полурабочая заготовка, не получается связать доп.данные файла с FileItem

--- src/app/form-element-render/component/upload-documents/upload-documents.component.ts
+++ src/app/form-element-render/component/upload-documents/upload-documents.component.ts
@@ -1,7 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { FileSelectDirective, FileDropDirective, FileUploader, FileItem } from 'ng2-file-upload/ng2-file-upload';
-import {FormControl, FormGroup} from '@angular/forms';
-import {ParsedResponseHeaders} from 'ng2-file-upload';
+import {ParsedResponseHeaders, Headers} from 'ng2-file-upload/ng2-file-upload';
+import {FormControl, FormGroup, FormArray} from '@angular/forms';
+import { ExtraFileData } from './extra-file-data';
 
 import { environment } from '../../../../environments/environment';
 
@@ -16,19 +17,28 @@ import { environment } from '../../../../environments/environment';
 export class UploadDocumentsComponent implements OnInit {
   public uploader: FileUploader = new FileUploader({
     url: URL,
-    additionalParameter: {
-      param1: 'test1',
-      param2: 'test2'
-    }
+    // additionalParameter: {
+    //   param1: 'test1',
+    //   param2: 'test2'
+    // },
+    headers: [{name: 'Accept', value: 'application/json'}]
   });
   // public hasBaseDropZoneOver = false;
   // public hasAnotherDropZoneOver = false;
-  public formElement = new FormGroup({
+  @Input()formElement = new FormGroup({
+    files: new FormGroup({})
   });
+
+  /**
+   * Уникальный сквозной индекс для идентификации файлов и связынных с ними сущностей.
+   * @type {number}
+   */
+  private uniqueIndex = 0;
+  private extraFileData: ExtraFileData[] = [];
 
   // file = new FormControl();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.uploader.onBuildItemForm = (fileItem, form) => this.onBuildItemForm(fileItem, form);
@@ -36,12 +46,34 @@ export class UploadDocumentsComponent implements OnInit {
     //   this.onCompleteItem(item, response, status, headers);
     this.uploader.onSuccessItem = (item: FileItem, response: string, status: number, headers: ParsedResponseHeaders) =>
        this.onSuccessItem(item, response, status, headers);
+
+    this.uploader.onAfterAddingFile = (fileItem: FileItem) => this.onAfterAddingFile(fileItem);
+
   }
 
-  onBuildItemForm(fileItem: FileItem, form: any) {
-    console.log('KOTA fileItem index= ', fileItem.index);
-    console.log('KOTA fileItem fileName = ', fileItem.file.name);
-    console.log('KOTA form = ', form);
+  onAfterAddingFile(fileItem: FileItem): any {
+    const data = new ExtraFileData(this.uniqueIndex++);
+    fileItem.formData.push(data);
+
+    console.log('KOTA onAfterAddingFile fileItem ', fileItem);
+
+    // if (fileItem.formData instanceof FormData === false) {
+    //   fileItem.formData = new FormData();
+    // }
+    // const uniqueIndex = this.uniqueIndex++;
+    // fileItem.formData.append('uniqueIndex', uniqueIndex);
+
+    // console.log('KOTA onAfterAddingFile fileItem ', fileItem.formData);
+    // console.log('KOTA onAfterAddingFile fileItem fileName = ', fileItem.file.name);
+    // console.log('KOTA onAfterAddingFile fileItem index = ', fileItem.formData.get('uniqueIndex'));
+  }
+
+  onBuildItemForm(fileItem: FileItem, form: any): void {
+    console.log('KOTA onBuildItemForm fileItem index= ', fileItem.index);
+    console.log('KOTA onBuildItemForm fileItem fileName = ', fileItem.file.name);
+    console.log('KOTA onBuildItemForm form = ', form);
+    // console.log('KOTA onBuildItemForm formData = ', fileItem.formData.get('uniqueIndex'));
+    form.append('uniqueIndex', '3333');
   }
 
   onCompleteItem(item: FileItem, response: string, status: number, headers: ParsedResponseHeaders): any {
@@ -62,4 +94,62 @@ export class UploadDocumentsComponent implements OnInit {
   // public fileOverAnother(e: any): void {
   //   this.hasAnotherDropZoneOver = e;
   // }
+
+  /**
+   * Сохраняем название документа для загрузки файла
+   * @param {FileItem} item
+   * @param {Event} event
+   */
+  saveDocumentName(item: FileItem, event: Event) {
+    // const target = <HTMLInputElement>event.target;
+    // if (item.formData instanceof FormData === false) {
+    //   item.formData = new FormData();
+    // }
+    // const formData = item.formData;
+    // if (formData.has('title')) {
+    //   formData.delete('title'); // т.к. не все браузеры поддерживают set()
+    // }
+    // formData.append('title', target.value);
+
+    //
+    //
+    // const index = item.formData.get('uniqueIndex');
+    // // console.log('KOTA saveDocumentName data', index, (event.target));
+    // for (let i = 0; i < this.extraFileData.length; ++i) {
+    //   if (this.extraFileData[i].index === index) {
+    //     this.extraFileData[i].title = target.value;
+    //     break;
+    //   }
+    // }
+    // const data = new ExtraFileData();
+    // data.index = index;
+    // data.title = target.value;
+    // this.extraFileData.push(data);
+  }
+
+  disabledUpload(item: FileItem): boolean {
+    const ret = item.isReady || item.isUploading || item.isSuccess || !this.hasDocName(item);
+    console.log('KOTA disabledUpload = ', ret);
+    return ret;
+  }
+  /**
+   * проверка что для заданного файла есть название документа
+   * @param {FileItem} item
+   * @returns {boolean}
+   */
+  hasDocName(item: FileItem): boolean {
+     return true;
+    // const ret = (item.formData.get('title') !== '');
+    // console.log('KOTA hasDocName ret', ret);
+    // return ret;
+
+    // const index = item.formData.get('uniqueIndex');
+    // for (let i = 0; i < this.extraFileData.length; ++i) {
+    //   if (this.extraFileData[i].index === index) {
+    //     console.log('KOTA hasDocName ', (this.extraFileData[i].title !== ''));
+    //     return (this.extraFileData[i].title !== '');
+    //   }
+    // }
+    // return false;
+  }
 }
