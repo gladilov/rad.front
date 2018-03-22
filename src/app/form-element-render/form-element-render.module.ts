3178e09de6216e32de364fd4ab755ba484b36337
Котрехов Андрей <kotrekhov@platformasoft.ru>
2018-03-22 11:26:24 +0300

    багфикс: запятая

--- src/app/form-element-render/form-element-render.module.ts
+++ src/app/form-element-render/form-element-render.module.ts
@@ -20,7 +20,7 @@ import { AgGridFormcontrolComponent } from './component/ag-grid-formcontrol/ag-g
     FormsModule,
     ReactiveFormsModule,
 
-    FileUploadModule
+    FileUploadModule,
     AgGridModule.withComponents([])
   ],
   declarations: [
